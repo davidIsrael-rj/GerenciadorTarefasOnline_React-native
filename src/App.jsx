94588f5229cbf8ceca862706f800eeb98106aaab
DavidIsrael <davidisrael.rj@gmail.com>
2025-03-03 11:29:49 -0300

    Iniciando o projeto Gerenciador de tarefas Off-line

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+export default props => {
+    return (
+        <>
+            <View style={styles.container}>
+                <Text style={styles.texto}>Gerenciador de Tarefas</Text>
+            </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#fff',
+    },
+    texto:{
+        fontSize:24,
+        fontWeight:"bold",        
+    }
+})
