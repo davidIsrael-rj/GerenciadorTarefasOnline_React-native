db800c61f44c794f4331747644393f3f934522a8
DavidIsrael <davidisrael.rj@gmail.com>
2025-03-04 20:16:57 -0300

    Criando o titulo

--- src/screens/TaskList.jsx
+++ src/screens/TaskList.jsx
@@ -3,12 +3,20 @@ import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 import todayImage from '../../assets/imgs/today.jpg'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 export default class TaskList extends Component {
+
     render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
                 <ImageBackground source={todayImage} style={styles.backgroud}>
-
+                    <View style={styles.titleBar}>
+                        <Text style={styles.textBar}>Hoje</Text>
+                        <Text style={styles.textBar}>{today}</Text>
+                    </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
                     <Text>TaskList</Text>
@@ -19,13 +27,22 @@ export default class TaskList extends Component {
 }
 
 const styles = StyleSheet.create({
-   container:{
-    flex:1
-   },
-   backgroud:{
-    flex:3
-   },
-   taskList:{
-    flex:7
-   }
+    container: {
+        flex: 1
+    },
+    backgroud: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        alignContent: "flex-end",
+        justifyContent: 'flex-end',
+    },
+    textBar: {
+        color: '#fff'
+
+    }
 })
