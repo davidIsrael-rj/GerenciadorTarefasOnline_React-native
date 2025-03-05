e25934c15e8f02a9c50c9636274a0e35cd296c28
DavidIsrael <davidisrael.rj@gmail.com>
2025-03-05 07:30:49 -0300

    Estilizando o componente Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default props => {
     return(
-       <View>
+       <View style={styles.container}>
             <Text>{props.desc}</Text>
             <Text>{props.estimateAt + ""}</Text>
             <Text>{props.doneAt === null ? '':props.doneAt +""}</Text>
        </View> 
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection:"row",
+        borderColor: '#AAA',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10
+    }
+})
