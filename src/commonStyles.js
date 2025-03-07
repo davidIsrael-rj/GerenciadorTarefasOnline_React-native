d2fcd8e6d4bb69f3f4cb4380222292a1a31bbd37
DavidIsrael <davidisrael.rj@gmail.com>
2025-03-07 19:06:32 -0300

    Criando espaço para o formulário de nova tarefa

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -1,6 +1,7 @@
 export default {
     fontFamily: 'Lato',
     colors:{
+        today: '#B13B44',
         secondary: '#FFF',
         mainText: '#808080',
         subText: '#A9A9A9'
--- src/screens/addTask.jsx
+++ src/screens/addTask.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Modal, StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import { Modal, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import commonStyles from "../commonStyles";
 
 export default class AddTask extends Component {
     render() {
@@ -7,6 +8,13 @@ export default class AddTask extends Component {
             <Modal transparent={true} visible={this.props.isVisible}
                 onRequestClose={this.props.onCancel}
                 animationType='slide'>
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                </View>
                 <TouchableWithoutFeedback
                     onPress={this.props.onCancel}>
                     <View style={styles.background}></View>
@@ -19,6 +27,18 @@ export default class AddTask extends Component {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.8)'
+        backgroundColor: 'rgba(0, 0, 0, 0.7)'
+    },
+    container:{
+        flex:2,
+        backgroundColor: '#fff'
+    },
+    header:{
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: "center",
+        padding: 15,
+        fontSize: 20
     }
 })
