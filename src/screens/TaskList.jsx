9b1f4e6fa582cb6783b2f96b45190069a266ded0
DavidIsrael <davidisrael.rj@gmail.com>
2025-03-07 18:51:33 -0300

    Adicionando uma tela Modal addTask

--- src/screens/TaskList.jsx
+++ src/screens/TaskList.jsx
@@ -17,11 +17,13 @@ import moment from 'moment'
 import 'moment/locale/pt-br'
 import Task from "../components/Task";
 import Icon from "react-native-vector-icons/FontAwesome";
+import AddTask from "./addTask";
 
 export default class TaskList extends Component {
 
     state = {
         showDoneTasks: true,
+        showAddTask: true,
         visibleTasks: [],
         tasks: [{
             id: Math.random(),
@@ -77,6 +79,9 @@ export default class TaskList extends Component {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask}
+                    onCancel={() => this.setState({showAddTask: false})}
+                />
                 <ImageBackground source={todayImage} style={styles.backgroud}>
                     <View style={styles.iconBar}>
                         <TouchableOpacity onPress={this.toggleFilter}>
