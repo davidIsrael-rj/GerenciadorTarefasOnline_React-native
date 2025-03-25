1dd4e091088cc71ceab12e45ce21c8f115a42f81
DavidIsrael <davidisrael.rj@gmail.com>
2025-03-25 06:11:20 -0300

    Formulário para criar a sua conta

--- src/screens/Auth.jsx
+++ src/screens/Auth.jsx
@@ -15,8 +15,11 @@ import commonStyles from '../commonStyles'
 export default class Auth extends Component {
 
     state = {
+        name: '',
         email: '',
-        password: ''
+        password: '',
+        confirmPassword: '',
+        stageNew: true
     }
 
     render() {
@@ -24,11 +27,23 @@ export default class Auth extends Component {
             <ImageBackground source={backgroundImage} style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
                 <View style={styles.formContainer}>
+                    <Text style={styles.subtitle}>
+                        {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+                    </Text>
+                    {this.state.stageNew &&
+                        <TextInput
+                            placeholder='Nome'
+                            placeholderTextColor='rgba(0,0,0,0.5)'
+                            value={this.state.name}
+                            style={styles.input}
+                            onChange={name => this.setState({ name })}
+                        />
+                    }
                     <TextInput
                         placeholder='E-Mail'
+                        placeholderTextColor='rgba(0,0,0,0.5)'
                         value={this.state.email}
                         style={styles.input}
-                        placeholderTextColor='rgba(0,0,0,0.5)'
                         onChangeText={email => this.setState({ email })}
                     />
                     <TextInput
@@ -37,10 +52,22 @@ export default class Auth extends Component {
                         value={this.state.password}
                         style={styles.input}
                         onChangeText={password => this.setState({ password })}
+                        secureTextEntry={true}
                     />
+                    {this.state.stageNew &&
+                        <TextInput
+                            placeholder='Confirmação de Senha'
+                            placeholderTextColor='rgba(0,0,0,0.5)'
+                            value={this.state.confirmPassword}
+                            style={styles.input}
+                            onChangeText={confirmPassword => this.setState({ confirmPassword })}
+                        />
+                    }
                     <TouchableOpacity>
                         <View style={styles.button}>
-                            <Text style={styles.buttonText}>Entrar</Text>
+                            <Text style={styles.buttonText}>
+                                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+                                </Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -62,6 +89,13 @@ const styles = StyleSheet.create({
         fontSize: 70,
         marginBottom: 10
     },
+    subtitle:{
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 10
+    },
     formContainer: {
         backgroundColor: 'rgba(0,0,0,0.8)',
         padding: 20,
@@ -73,14 +107,14 @@ const styles = StyleSheet.create({
         color: '#000',
         padding: Platform.OS == 'ios' ? 15 : 10
     },
-    button:{
-        backgroundColor:'#080',
+    button: {
+        backgroundColor: '#080',
         marginTop: 10,
         padding: 10,
         alignItems: 'center'
 
     },
-    buttonText:{
+    buttonText: {
         fontFamily: commonStyles.fontFamily,
         color: '#FFF',
         fontSize: 20
