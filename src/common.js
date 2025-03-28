1d70311dd86f89a1cda415c63b492a4116e2a6ed
DavidIsrael <davidisrael.rj@gmail.com>
2025-03-28 20:07:09 -0300

    Definindo o caminho para o backend uma função de erro e de sucesso

---
+++ src/common.js
@@ -0,0 +1,16 @@
+import { Alert, Platform } from "react-native";
+
+const server = Platform.OS === 'ios'
+    ? 'http://localhost:3000' : 'http://localhost:3000'
+
+    // para android pode ser esse caminho http://10.0.2.2:3000
+
+    function showError(err){
+    Alert.alert('Ops! Ocorreu um Probema!', `Mensagem: ${err}`)
+}
+
+function showSuccess(msg){
+    Alert.alert('Sucesso!', msg)
+}
+
+export {server, showError, showSuccess}
